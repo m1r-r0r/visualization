2c1d4de8ff3bf19d0de14a490d2239a2a9cd1a21
m1r-r0r <toolexe@gmail.com>
2023-12-25 15:44:20 +0300

    init_spring

---
+++ src/test/java/com/github/m1rr0r/visualization/VisualizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.m1rr0r.visualization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
